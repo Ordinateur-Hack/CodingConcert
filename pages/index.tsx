c6e50dd0ea940ff5f23c35e6f4f6a56eaa8afe45
Dominic Plein <dominic-plein@gmx.de>
2020-03-16 18:28:54 +0100

    Init stepper for Live Reg-Sheets-Viewer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
-import { Container, Box, Typography, Divider, makeStyles, Button, Theme } from '@material-ui/core';
+import { Container, Box, Typography, Divider } from '@material-ui/core';
 import { getLayout as getSiteLayout } from '../src/Layouts/SiteLayout';
-import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
 import Link from '../src/Link';
-import Dropzone from '../src/Dropzone/Dropzone';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  button: {
-    boxShadow: theme.shadows[12],
-    width: '100%',
-    marginTop: theme.spacing(2),
-    padding: theme.spacing(2),
-    background: `linear-gradient(45deg, ${theme.palette.secondary.light} 30%, #FF8E53 90%)`
-  }
-}));
+import RegSheetsStepper from '../src/RegSheetsStepper';
 
 const Index = () => {
-  const classes = useStyles();
-
   return (
     <Container maxWidth="sm">
       <Box my={4}>
 
         <Typography variant="h4" component="h1" gutterBottom>
-          Live RegFileViewer
+          Live Reg-Sheets-Viewer
         </Typography>
         <Link href="/about">
           About this project
         </Link>
         <Divider style={{ margin: "16px 0" }} />
-        <Dropzone />
-        <Button
-          variant="contained"
-          color="primary"
-          centerRipple
-          className={classes.button}
-          endIcon={<PlayCircleFilledWhiteIcon />}>
-          Start PDF Viewer
-        </Button>
+        <RegSheetsStepper />
 
       </Box>
     </Container>
